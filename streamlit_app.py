ebc44f4943cd42d59e0bdd90f50ac633202586b0
sharla martiza yunani <94608562+caramelala@users.noreply.github.com>
2024-06-21 00:15:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
- import pandas as pd
- from sklearn.model_selection import train_test_split
- from sklearn.linear_model import LogisticRegression
- from sklearn.metrics import accuracy_score
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 # print first 5 rows of the dataset
 heart_data.head()
